16dde5c4c20b0738e0b40427398026e0278b9df9
KISHAN.PATEL2@coxautoinc.com <KISHAN.PATEL2@coxautoinc.com>
2024-11-11 18:38:22 -0500

    logger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import express from "express";
+require('dotenv').config()
+import logger from "./dependencies/logger";
+
 import movieRoutes from "./routes/movieRoutes";
+
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
 app.use("/api", movieRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    logger.info(`Server is running on port ${PORT}`);
 });
